3850822dd4fe5fb9c8b3616409e9d2ac46a9dcba
l05 <285296372@qq.com>
2019-01-16 16:20:40 +0800

    template

---
+++ src/main/java/com/zdpang/template/config/Swagger2.java
@@ -0,0 +1,46 @@
+package com.zdpang.template.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    @Bean
+    public Docket createRestApi() {
+        //添加head参数start
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<Parameter>();
+        tokenPar.name("Authorization").description("令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        pars.add(tokenPar.build());
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.zdpang.template"))
+                .paths(PathSelectors.any())
+                .build()
+                .globalOperationParameters(pars);
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("springboot利用swagger构建api文档")
+                .description("简单优雅的restfun风格，https://www.jianshu.com/u/cbba9a1a8f0a")
+                .termsOfServiceUrl("https://www.jianshu.com/u/cbba9a1a8f0a")
+                .version("1.0")
+                .build();
+    }
+}
---
+++ src/main/java/com/zdpang/template/generator/MpGenerator.java
@@ -0,0 +1,64 @@
+package com.zdpang.template.generator;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.zdpang.template.util.PathUtil;
+
+public class MpGenerator {
+
+    public static void main(String[] args) {
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig()
+        .setOutputDir(PathUtil.getJarPath() + "/src/main/java/com/zdpang/template/generator/")
+        .setFileOverride(true)
+        .setActiveRecord(true)// 不需要ActiveRecord特性的请改为false
+        .setEnableCache(false)// XML 二级缓存
+        .setBaseResultMap(true)// XML ResultMap
+        .setBaseColumnList(false)// XML columList
+        .setIdType(IdType.UUID)
+        .setServiceName("%sService")
+        .setAuthor("Gumh");
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("root");
+        dsc.setUrl("jdbc:mysql://localhost:3306/env?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+        mpg.setDataSource(dsc);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig()
+        .setCapitalMode(true)// 全局大写命名 ORACLE 注意
+        .setNaming(NamingStrategy.underline_to_camel)// 表名生成策略
+        /**
+         * 只需修改表名
+         */
+        .setInclude(new String[] { "sys_user" }); // 需要生成的表
+        mpg.setStrategy(strategy);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig()
+        .setParent("")
+        .setMapper("dao")
+        .setService("service")
+        .setController("controller")
+        .setEntity("model")
+        .setXml("dao");
+        mpg.setPackageInfo(pc);
+
+        // 执行生成
+        mpg.execute();
+    }
+
+}
