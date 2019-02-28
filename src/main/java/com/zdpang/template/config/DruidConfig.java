3c2fe389ef513166eca53456f370f2f9055cd0ea
l05 <285296372@qq.com>
2019-02-28 18:54:10 +0800

    集成sharding-jdbc部分

--- src/main/java/com/zdpang/template/config/DruidConfig.java
+++ src/main/java/com/zdpang/template/config/DruidConfig.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @Configuration
 public class DruidConfig {
 
-    @ConfigurationProperties(prefix = "spring.datasource")
+    @ConfigurationProperties(prefix = "sharding.jdbc.datasource.ds_master")
     @Bean
     public DataSource druid(){
        return  new DruidDataSource();
