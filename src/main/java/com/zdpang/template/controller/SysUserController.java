e1b531b5bf2c2f2d4ef70a39629395e2f3e78039
UncleWangKing <285296372@qq.com>
2019-05-24 01:38:14 +0800

    mybatis 注解用法

--- src/main/java/com/zdpang/template/controller/SysUserController.java
+++ src/main/java/com/zdpang/template/controller/SysUserController.java
@@ -3,6 +3,7 @@ package com.zdpang.template.controller;
 
 import com.zdpang.template.bean.ResponseBean;
 import com.zdpang.template.common.CrudController;
+import com.zdpang.template.dao.SysUserMapper;
 import com.zdpang.template.model.SysUser;
 import com.zdpang.template.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,9 +25,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class SysUserController implements CrudController<SysUser, SysUserService> {
     @Autowired
     SysUserService sysUserService;
+    @Autowired
+    SysUserMapper sysUserMapper;
     @Override
     public SysUserService getService() {
         return sysUserService;
+    }
+
+    @GetMapping(value="/selectByAnno")
+    ResponseBean selectByAnno(String id) throws Exception {
+
+        return new ResponseBean().success(sysUserMapper.selectByAnno(id));
     }
 
     @GetMapping(value="/sqlatk")
--- src/main/java/com/zdpang/template/dao/SysUserMapper.java
+++ src/main/java/com/zdpang/template/dao/SysUserMapper.java
@@ -3,6 +3,11 @@ package com.zdpang.template.dao;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zdpang.template.model.SysUser;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,5 +19,23 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface SysUserMapper extends BaseMapper<SysUser> {
-
+    /**
+     * 根据用户Id返回用户数据
+     * @param userId 用户Id
+     * @return 用户数据
+     */
+    @Results({
+            @Result(property = "id", column = "id", id = true),
+            @Result(property = "username", column = "username"),
+            @Result(property = "password", column = "password")
+    })
+    @Select({ "SELECT "
+            + " id , "
+            + " username , "
+            + " password "
+            + " FROM "
+            + " sys_user "
+            + " WHERE "
+            + " id = #{userId}" })
+    SysUser selectByAnno(String userId);
 }
